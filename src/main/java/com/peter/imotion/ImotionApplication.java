903214e9be3135f92bc7d4f226fca9aaaed6dda8
haiyi <hwang866@wisc.edu>
2024-04-16 15:36:08 +0100

    create springboot application

---
+++ src/main/java/com/peter/imotion/ImotionApplication.java
@@ -0,0 +1,13 @@
+package com.peter.imotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peter/imotion/ImotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.imotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
