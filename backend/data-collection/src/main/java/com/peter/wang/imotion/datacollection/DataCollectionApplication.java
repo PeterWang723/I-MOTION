8d8ed9369a8241d8a65d5bdd6fe61d854a1b970a
haiyi <pw723@ic.ac.uk>
2024-05-15 23:19:34 +0800

    add data collection api

---
+++ backend/data-collection/src/main/java/com/peter/wang/imotion/datacollection/DataCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.peter.wang.imotion.datacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectionApplication.class, args);
+	}
+
+}
---
+++ backend/data-collection/src/test/java/com/peter/wang/imotion/datacollection/DataCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.wang.imotion.datacollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
