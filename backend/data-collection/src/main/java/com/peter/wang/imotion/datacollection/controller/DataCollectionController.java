7e3a61b43d1199e66c9741965841986a28d15d3e
haiyi <pw723@ic.ac.uk>
2024-06-27 16:15:05 +0100

    update

--- backend/data-collection/src/main/java/com/peter/wang/imotion/datacollection/controller/DataCollectionController.java
+++ backend/data-collection/src/main/java/com/peter/wang/imotion/datacollection/controller/DataCollectionController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -32,33 +33,39 @@ public class DataCollectionController {
     @PostMapping("/save_loc")
     public Response<String> saveLocation(@RequestHeader("username") String username, @Valid @RequestBody List<Location> locations){
         Long u_id = Long.parseLong(username);
-        for (Location location : locations) {
-            location.setUid(u_id);
-        }
+        locations.forEach(location -> location.setUid(u_id));
         locationRepository.saveAll(locations);
         return new Response<>(ReturnCode.SUCCESS);
     }
 
     @PostMapping("/save_acc")
     public Response<String> saveAcc(@RequestHeader("username") String username, @Valid @RequestBody List<Acceleratometer> acceleratometers){
+        Long u_id = Long.parseLong(username);
+        acceleratometers.forEach(acc -> acc.setUid(u_id));
         acceleratometerRepository.saveAll(acceleratometers);
         return new Response<>(ReturnCode.SUCCESS);
     }
 
     @PostMapping("/save_data")
     public Response<String> saveData(@RequestHeader("username") String username, @Valid @RequestBody List<DataUsage> dataUsages){
+        Long u_id = Long.parseLong(username);
+        dataUsages.forEach(dataUsage -> dataUsage.setUid(u_id));
         dataUsageRepository.saveAll(dataUsages);
         return new Response<>(ReturnCode.SUCCESS);
     }
 
     @PostMapping("/save_energy")
     public Response<String> saveEnergy(@RequestHeader("username") String username, @Valid @RequestBody List<CarEnergyUsage> carEnergyUsages){
+        Long u_id = Long.parseLong(username);
+        carEnergyUsages.forEach(carEnergy -> carEnergy.setUid(u_id));
         carEnergyRepository.saveAll(carEnergyUsages);
         return new Response<>(ReturnCode.SUCCESS);
     }
 
     @PostMapping("/save_health")
     public Response<String> saveHealth(@RequestHeader("username") String username, @Valid @RequestBody List<Health> healths){
+        Long u_id = Long.parseLong(username);
+        healths.forEach(health -> health.setUid(u_id));
         healthRepository.saveAll(healths);
         return new Response<>(ReturnCode.SUCCESS);
     }
